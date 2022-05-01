ad9c573ef0453397fc044b62e02082d30346bba1
Nicholas-Cabrera <78450719+Nicholas-Cabrera@users.noreply.github.com>
2022-04-30 22:51:16 -0400

    html and landing page initial work

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
  * handles window resizes.
  *
  */
-import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
+import { WebGLRenderer, PerspectiveCamera, Vector3, Box2 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CubeScenes, Launch } from 'scenes';
 
@@ -33,7 +33,76 @@ const canvas = renderer.domElement;
 canvas.style.display = 'block'; // Removes padding below canvas
 document.body.style.margin = 0; // Removes margin around page
 document.body.style.overflow = 'hidden'; // Fix scrolling
+
+let htmlStyle = '.slidecontainer {width: 100%;}' +
+'.slider {-webkit-appearance: none; appearance: none; width: 100%; height: 25px; background: #d3d3d3; outline: none; opacity: 0.7; -webkit-transition: .2s; transition: opacity .2s;}' +
+'.slider:hover {opacity: 1;}' +
+'.slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none;width: 25px; height: 25px; background: #04AA6D; cursor: pointer;}' +
+'.slider::-moz-range-thumb {width: 25px; +height: 25px; background: #04AA6D; cursor: pointer; }';
+
+let html =
+'<center><h2>How to Play:</h2></center>' +
+'<p>This game takes the classic Minesweeper and moves it to a 3D cube. When you click on a block you can reveal either a number, an empty space, or a bomb. Your goal is to reveal every block without a bomb inside. Numbered blocks will tell you whether there is a bomb anywhere in its vicinity (anywhere in the 3x3 space centered around that cube). However, if you reveal a bomb you lose and the game ends. </p>' +
+'<center><h3>Controls</h3></center>' +
+'<p>Left Click and Drag: Rotate cube</p>' +
+'<p>Left Click Cube: Reveal highlighted block</p>' +
+'<p>Right Click: Flag highlighted block (right click again to change or remove flag)</p>' +
+'<div class="minenumber">' +
+'<center><h2>Choose Cube\'s Size:</h2></center>' +
+'<input type="range" min="5" max="50" value="30" class="slider" id="myRange">'+
+'</div>'+
+'<center><h2>Cube Size: <span id="demo"></span> x <span id="demo2"></span> x <span id="demo3"></span></h2></center>' +
+'<div class="diffSetting">' +
+    '<center><h2>Choose Your Difficulty:</h2>'+
+    '<h2>'+
+    '<input type="radio" id="diff1" name="diff" value="1">'+
+    '<label for="diff1" style="color:green">Easy</label><br>'+
+    '<input type="radio" id="diff2" name="diff" value="2" checked>'+
+    '<label for="diff2" style="color:yellow">Medium</label><br>'+
+    '<input type="radio" id="diff3" name="diff" value="3">'+
+    '<label for="diff3" style="color:red">Hard</label><br>'+
+'</h2>'+
+'</center>'+
+'</div>'+
+'<div class="diffSetting">'+
+'<center><h2>Choose Block Settings:</h2>'+
+'<h2>'+
+'<input type="radio" id="hollow" name="cubeSetting" value="1" checked>'+
+'<label for="diff1">Hollow Cube (only play on cube faces)</label><br>'+
+'<input type="radio" id="diff2" name="cubeSetting" value="2">'+
+'<label for="diff2">Filled Cube (play on the inside of the cube)</label><br>'+
+'</h2>'+
+'</center>'+
+'</div>'+
+'<button onclick="setScene()">Click Here to Begin!</button>'+
+'<script>' +
+'var slider = document.getElementById("myRange");' +
+'var output = document.getElementById("demo");' +
+'var output2 = document.getElementById("demo2");' +
+'var output3 = document.getElementById("demo3");' +
+'output.innerHTML = slider.value;' +
+'output2.innerHTML = slider.value;' +
+'output3.innerHTML = document.slider.value;' +
+'slider.oninput = function() {' +
+'output.innerHTML = this.value;' +
+'output2.innerHTML = this.value;' +
+'output3.innerHTML = this.value;}' +
+'</script>';
+
+
+let style = document.createElement("style");
+style.innerHTML = htmlStyle;
+
+let page = document.createElement("BODY");
+page.innerHTML = html;
+page.style.cssText = 'background: #9e9e9e;';
+
+document.head.appendChild(style);
+document.body = page;
+var slideTest = document.getElementById("slider");
+console.log(slideTest);
 document.body.appendChild(canvas);
+
 
 // Set up controls
 const controls = new OrbitControls(camera, canvas);
